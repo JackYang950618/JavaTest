f949e1de55cc1ccee023718ddb5786389c93315a
JackYang950618 <1169267380@qq.com>
2018-06-12 20:15:58 +0800

    第一次修改

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("修改了！");
         System.out.println("hello world!");
     }
 }
